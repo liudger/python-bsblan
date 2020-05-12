01323c279bfaf2d32034020327bf603c6b0d2831
Willem-Jan van Rootselaar <liudgervr@gmail.com>
2020-05-12 19:37:10 +0200

    Removed params not needed
    to prevent other system missing keys

--- bsblan/models.py
+++ bsblan/models.py
@@ -2,55 +2,25 @@
 
 import attr
 
-# info to know what datatypes to expect for "value"
-# "DataType" (
-# 0 = Zahl,
-# 1 = ENUM,
-# 2 = Bit-Wert (Dezimalwert gefolgt von Bitmaske gefolgt von ausgewählter Option),
-# 3 = Wochentag,
-# 4 = Stunde/Minute,
-# 5 = Datum/Uhrzeit,
-# 6 = Tag/Monat,
-# 7 = String,
-# 8 = PPS-Uhrzeit (Wochentag, Stunde:Minute))
-#
-# &deg;C is the return of the unit
-# @attr.s(auto_attribs=True, frozen=True)
-# class Scan:
-#     """Object Scans the BSBlan params."""
-
-#     pass
-
 
 @attr.s(auto_attribs=True, frozen=True)
 class State:
     """Object holding the BSBlan state."""
 
-    # away_mode: int
-    cooling_setpoint: float
-    current_heatpump_mode: str
     current_hvac_mode: int
-    current_hvac_operation: str
     current_temperature: float
     target_temperature: float
     temperature_unit: str
-    switching_mode: bool
 
     @staticmethod
     def from_dict(data: dict):
         """Return State object from BSBLan API response."""
 
-        # no desc if it needs automatic translation
         return State(
-            # away_mode=data["703"]["value"],  # 256=home , 257=away
-            cooling_setpoint=data["912"]["value"],
-            current_heatpump_mode=data["8006"]["desc"],
             current_hvac_mode=data["700"]["value"],
-            current_hvac_operation=data["8000"]["desc"],  # this can also stay
             current_temperature=data["8740"]["value"],
             target_temperature=data["710"]["value"],
             temperature_unit=data["8740"]["unit"],
-            switching_mode=data["969"]["value"],
         )
 
 
