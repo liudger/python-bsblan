b670af8c32da60fe307a2d959be5747f1ef81097
Willem-Jan L. van Rootselaar <liudgervr@gmail.com>
2022-11-13 11:28:31 +0100

    update event loop
    python 3.10

--- examples/control.py
+++ examples/control.py
@@ -48,5 +48,5 @@ async def main():
 
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
+    loop = asyncio.get_event_loop_policy().get_event_loop()
     loop.run_until_complete(main())
