8c3091932a5678963fc515e4ed286568ed63e55d
Willem-Jan L. van Rootselaar <liudgervr@gmail.com>
2022-11-15 20:50:10 +0100

    add staticState

--- src/bsblan/__init__.py
+++ src/bsblan/__init__.py
@@ -1,7 +1,7 @@
 """Asynchronous Python client for BSBLAN."""
 
 from .bsblan import BSBLAN, BSBLANConnectionError, BSBLANError
-from .models import Device, Info, Sensor, State
+from .models import Device, Info, Sensor, State, StaticState
 
 __all__ = [
     "BSBLAN",
@@ -11,4 +11,5 @@ __all__ = [
     "State",
     "Device",
     "Sensor",
+    "StaticState",
 ]
